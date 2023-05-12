8a56129af1b0576a9f4618754578edafc3bf7e7a
Marx-wrld <iongonga@kabarak.ac.ke>
2023-05-12 14:44:06 +0300

    footer component and Bottom bar

--- real-estate/pages/index.js
+++ real-estate/pages/index.js
@@ -1,9 +1,11 @@
 import Navigation from "@/features/common/modules/Navigation/Navigation";
+import Footer from "@/features/common/modules/Footer/Footer"
 
 export default function Home() {
   return (
     <>
       <Navigation />
+      <Footer />
     </>
   )
 }
