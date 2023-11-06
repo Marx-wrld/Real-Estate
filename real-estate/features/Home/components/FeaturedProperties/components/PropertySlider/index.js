0eebc0f870219126929188b8edadd480ee80939c
Marx-wrld <iongonga@kabarak.ac.ke>
2023-11-06 17:04:23 +0300

    App rebuild with vite and MERN stack

--- real-estate/features/Home/components/FeaturedProperties/components/PropertySlider/index.js
+++
@@ -1,2 +0,0 @@
-export {default} from "./PropertySlider";
-//exporting from the sibling PropertySlider
--- real-estate/features/common/modules/Navigation/navigationConsts.js
+++
@@ -1,7 +0,0 @@
-import { HiPhoneArrowUpRight } from "react-icons/hi2";
-
-export const navigationLinks = [
-    {title: 'Properties', link: '/properties'},
-    {title: 'Contact', link: '/contact'},
-    {title: '(254) 555 555', link: 'tel:(254) 555 555', icon: <HiPhoneArrowUpRight />}
-]
