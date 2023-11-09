232d5a437d22ceee1a9e7efd5ab774e6e77bec90
Marx-wrld <iongonga@kabarak.ac.ke>
2023-11-09 10:41:31 +0300

    Complete signin page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -10,7 +10,7 @@ export const signup = async (req, res, next) => {
 
     try {
         await newUser.save()
-        res.status(201).json("User created successfully");
+        res.status(201).json("User created successfully!");
     } catch (error) {
         next(error);
     } 
@@ -21,9 +21,9 @@ export const signin = async (req, res, next) => {
     const { email, password } = req.body;
     try {
         const validUser = await User.findOne({ email });
-        if (!validUser) return next(errorHandler(400, "User not found"));
+        if (!validUser) return next(errorHandler(400, "User not found!"));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if (!validPassword) return next(errorHandler(401, "Invalid credentials"));
+        if (!validPassword) return next(errorHandler(401, "Invalid credentials!"));
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
         const { password: pass, ...rest } = validUser._doc;
         res
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,8 +1,66 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const SignIn = () => {
+  const [formData, setFormData] = useState({}); //state to keep track of all the changes in the form
+  const [error, setError] = useState(null); //state to keep track of all the errors
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData (
+      {
+        ...formData,
+        [e.target.id] : e.target.value,
+      }
+    )
+  } //function to handle the changes in the form
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signin', 
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+    
+  };
+
   return (
-    <div>SignIn</div>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className='text-3xl text-center font-semibold m-7'>Sign In</h1>
+      
+      <form onSubmit={handleSubmit} action="" className="flex flex-col gap-4">
+        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
+        <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">{loading ? 'Loading...': 'Sign In'}</button>
+      </form>
+      <div disabled={loading} className="flex gap-2 mt-5 mb-5">
+        <p>Don&apos;t have an account?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700">Sign Up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 text-center">{error}</p>}
+    </div>
   )
 }
 
-export default SignIn
+export default SignIn 
