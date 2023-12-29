b61b7d40a330b54cf47cd0eb57ec9273abe5930b
Marx-wrld <iongonga@kabarak.ac.ke>
2023-12-29 14:17:08 +0300

    Styling and minor props validation bug fixes

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
 
 const Contact = ({ listing }) => {
     const [landlord, setLandlord] = useState(null);
@@ -20,7 +21,6 @@ const Contact = ({ listing }) => {
         fetchLandlord();
     }, [listing.userRef]);
 
-
   return (
     <>
         { landlord && (
@@ -29,7 +29,7 @@ const Contact = ({ listing }) => {
                     <img src={landlord.avatar} alt="avatar" className="w-12 h-12 rounded-full"/>
                     <p className="ml-2"> Contact <span className="font-semibold">{landlord.username} </span>for <span className="font-semibold">{listing.name.toLowerCase()}</span></p>
                 </div>
-                <textarea name="message" id="messsage" rows="2" value={message} onChange={onChange} placeholder="Enter your message" className="w-full border p-3 rounded-lg"></textarea>
+                <textarea name="message" id="messsage" rows="2" value={message} onChange={onChange} placeholder="Enter your message" className="w-full border-1 p-3 rounded-lg resize-none" style={{ height: '80px' }}></textarea>
                 <Link to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`} className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95">
                     Send Message
                 </Link>
@@ -39,4 +39,13 @@ const Contact = ({ listing }) => {
   )
 };
 
+Contact.propTypes = {
+    listing: PropTypes.shape({
+        userRef: PropTypes.string,
+        name: PropTypes.string,
+    }).isRequired,
+};
+
+
 export default Contact
+
