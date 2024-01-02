bdab74866f01317c54041e7a6fb6c52ffb2774b9
Marx-wrld <iongonga@kabarak.ac.ke>
2024-01-02 14:14:30 +0300

    Complete About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="py-20 px-10 max-2-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About Kana Estate</h1>
+      <p className="mb-4 text-slate-700">
+        Kana Estate is your trusted partner in the realm of real estate, dedicated to providing unparalleled services and expertise in the property market. With a commitment to excellence, we specialize in connecting individuals with their dream homes and investment opportunities. Our extensive portfolio encompasses a diverse range of listings, from luxurious residences to commercial spaces, ensuring that clients find the perfect property tailored to their unique needs and aspirations.
+      </p>
+
+      <p className="mb-4 text-slate-700">
+        At the heart of Kana Estate is a team of seasoned professionals who bring a wealth of experience and industry knowledge to the table. We understand that real estate transactions can be complex, and our dedicated experts are here to guide you every step of the way. Whether you&apos;re buying, selling, or investing, our goal is to make the process seamless and rewarding. We take pride in fostering long-term relationships with our clients, built on trust, transparency, and a shared vision of realizing their real estate goals.
+      </p>
+
+      <p className="mb-4 text-slate-700">
+        Beyond our commitment to exceptional service, Kana Estate stands out for its innovation and forward-thinking approach. Leveraging cutting-edge technology and market insights, we stay ahead of trends to provide clients with a competitive edge. Whether you&apos;re a first-time homebuyer, a seasoned investor, or a business owner seeking prime commercial space, Kana Estate is your gateway to a world of real estate opportunities. Discover the difference of working with a dynamic and client-centric agency—welcome to Kana Estate, where your property journey begins.
+      </p>
+    </div>
   )
 }
 
